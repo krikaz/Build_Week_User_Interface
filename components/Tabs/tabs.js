b31dac7886774ffa0e4a89a40e35c939545a8c6c
krikaz <pascal.bou@protonmail.com>
2019-05-20 16:41:45 +0200

    added JS linked tab-genre to book-genre

--- components/Tabs/tabs.js
+++ components/Tabs/tabs.js
@@ -1,8 +1,9 @@
 class Tab {
   constructor(tabElement) {
+    // debugger
     this.tabElement = tabElement;
     this.genre = this.tabElement.dataset.genre;
-    // console.log(this.genre);
+
     if (this.genre === "all") {
       this.books = document.querySelectorAll(".book");
     } else {
@@ -10,43 +11,38 @@ class Tab {
         `.book[data-genre='${this.genre}']`
       );
     }
+    this.books.forEach(book => book.classList.add('book-show'));
     // console.log(this.books);
-    // this.books.forEach(book => new Book(book));
-    this.books.forEach(book => book.classList.add("book-show"));
-    this.tabElement.addEventListener("click", this.select);
+
+    this.books = Array.from(this.books).map(book => new Book(book));
+    // console.log(this.books);
+
+    this.tabElement.addEventListener("click", () => this.select());
   }
 
   select() {
     tabs.forEach(tab => tab.classList.remove("tab-selected"));
-    this.classList.add("tab-selected");
+    this.tabElement.classList.add("tab-selected");
 
-    this.books.forEach(book => book.classList.remove("book-show"));
-    if (this.genre === "all") {
-      this.books = document.querySelectorAll(".book");
-    } else {
-      this.books = document.querySelectorAll(
-        `.book[data-genre='${this.genre}']`
-      );
-    }
     // console.log(this.books);
-    // this.books.forEach(book => new Book(book));
-    this.books.forEach(book => book.classList.add("book-show"));
+    const books = document.querySelectorAll(".book");
+    books.forEach(book => book.classList.remove("book-show"));
+
+    this.books.forEach(book => book.select());
+
   }
 }
 
-// class Book {
-//   constructor(bookElement) {
-//     this.bookElement = bookElement;
-//     console.log(this.bookElement);
-//   }
+class Book {
+  constructor(bookElement) {
+    this.bookElement = bookElement;
+    // console.log(this.bookElement);
+  }
 
-//   select() {
-//     this.bookElement.classList('book-show');
-//   }
-// }
+  select() {
+    this.bookElement.classList.add("book-show");
+  }
+}
 
 const tabs = document.querySelectorAll(".tab-link");
-// console.log(tabs);
-// const newTabs = Array.from(tabs);
-// console.log(newTabs);
 tabs.forEach(tab => new Tab(tab));
