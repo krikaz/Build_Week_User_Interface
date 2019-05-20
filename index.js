2f0b9f502233a6eadb65b249e9f3f9c974d185c2
krikaz <pascal.bou@protonmail.com>
2019-05-20 14:25:19 +0200

    added html & css for nav, left-menu and 4 books. Js color for left-menu

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const test = document.createElement('h1');
-test.textContent = 'this is an h1 created with js';
-document.body.appendChild(test);
+class Tab {
+  constructor(tab) {
+    this.tab = tab;
+    this.tab.addEventListener('click', this.select);
+  }
+
+  select() {
+    tabs.forEach(tab => tab.classList.remove('tab-selected'));
+    this.classList.add('tab-selected');
+  }
+}
+
+const tabs = document.querySelectorAll('.tab-link');
+// console.log(tabs);
+// const newTabs = Array.from(tabs);
+// console.log(newTabs);
+tabs.forEach(tab => new Tab(tab));
+
+
+
