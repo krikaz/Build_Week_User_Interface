46b213dae49fefa14a0a6547b49d402c5c036278
krikaz <pascal.bou@protonmail.com>
2019-05-21 10:22:57 +0200

    added CSS for anchor tags

---
+++ components/Nav/nav.js
@@ -0,0 +1,17 @@
+// class Anchor {
+//   constructor(anchorElement) {
+//     this.anchorElement = anchorElement;
+//     this.anchorElement.addEventListener('mouseover', () => this.hover());
+
+
+//   }
+
+//   hover() {
+//     // console.log('test');
+//     this.anchorElement.style['background-color'] = 'black';
+//   }
+// }
+
+// const anchors = document.querySelectorAll('header nav a');
+// anchors.forEach(anchor => new Anchor(anchor));
+// // console.log(anchors);
