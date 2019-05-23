b4ba5a71569a80288545be2ae0513971bcd19fc3
krikaz <pascal.bou@protonmail.com>
2019-05-23 13:46:17 +0200

    refactored nav.js

--- components/Nav/nav.js
+++ components/Nav/nav.js
@@ -1,49 +1,38 @@
-const about = document.querySelector('.about');
 const allTabs = document.querySelector('.tabs');
 const allBooks = document.querySelector('.books');
 const allBookCards = Array.from(document.querySelectorAll('.book'));
 const aboutContent = document.querySelector('.aboutContent');
 const randomContent = document.querySelector('.randomContent');
 
-// console.log(about);
-// console.log(allTabs);
-// console.log(allBooks);
-// console.log(allBookCards);
-
-
+const about = document.querySelector('.about');
 about.addEventListener('click', () => aboutMe());
-
-function aboutMe() {
-  allTabs.classList.add('hidden');
-  allBooks.classList.add('hidden');
-  allBookCards.forEach(bookCard => bookCard.classList.remove('book-show'));
-  aboutContent.classList.remove('hidden');
-  randomContent.classList.add('hidden');
-  
-}
-
 
 const random = document.querySelector('.random');
 random.addEventListener('click', () => randomize());
 
+function aboutMe() {
+  hideAll();
+  aboutContent.classList.remove('hidden');
+  randomContent.classList.add('hidden');
+}
+
 function randomize() {
-  allTabs.classList.add('hidden');
-  allBooks.classList.add('hidden');
-  allBookCards.forEach(bookCard => bookCard.classList.remove('book-show'));
+  hideAll()
   aboutContent.classList.add('hidden');
   randomContent.classList.remove('hidden');
-
   pickBook();
 
 }
 
 function pickBook() {
   const randomNumber = (Math.floor(Math.random() * 12)+1).toString();
-  console.log(randomNumber);
-  // console.log(2);
-  // console.log('2');
   const pickedBook = document.querySelector(`.book[data-id='${randomNumber}']`)
-  console.log(pickedBook);
   pickedBook.classList.add('book-show');
+}
+
+function hideAll() {
+  allTabs.classList.add('hidden');
+  allBooks.classList.add('hidden');
+  allBookCards.forEach(bookCard => bookCard.classList.remove('book-show'));
 }
 
